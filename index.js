94134d189aa9c597419c2db9805f18b602c41d48
GUYOT Julie <guyotjulie@MacBook-Pro-de-GUYOT.local>
2021-03-29 10:31:54 +0200

    cors

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(collectionRoutes);
 const collectionDetailsRoutes = require("./routes/collectionDetails");
 app.use(collectionDetailsRoutes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
